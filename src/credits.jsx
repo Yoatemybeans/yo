fdf8a9385a03e3261273d51d9c8746c424a372c4
yoatemybeans <abhivaluveri@gmail.com>
2023-01-21 10:25:14 -0500

    initialise project

---
+++ src/credits.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Credits from './creditsapp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('Credits')).render(
+  <React.StrictMode>
+    <Credits/>
+  </React.StrictMode>,
+)
---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+		colors: {
+			primary: '#C8B568'
+		},
+
+		
+	},
+	fontFamily: {
+		sans: ['Montserrat', 'sans-serif'],
+	}
+  },
+  plugins: [],
+}

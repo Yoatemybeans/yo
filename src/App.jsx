643be83daf374c0a8821d4a7d1287e43918dcb22
yoatemybeans <abhivaluveri@gmail.com>
2023-01-21 11:18:07 -0500

    Initialise project

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
 		<div className="min-h-screen flex flex-col text-gray-800">
 			<main className="container mx-auto px-6 pt-6 flex-1 text-center">
       			<img src="src\assets\For Web\png\Color logo - no background.png" alt="" height="100rem" width="20%"></img>
+				
 				<h3 className="text-2xl md:text-4xl lg:text-6xl uppercase">The Next Level of</h3>
 				<h2 className="text-3xl md:text-6xl lg:text-8xl uppercase font-black mb-8">Dropshipping</h2>
 
@@ -20,10 +21,7 @@ function App() {
 					id="storox-form"
 					name="storox-form"
 					action="https://sheetdb.io/api/v1/7khjhbg6k1vtb"
-					target="hiddenFrame"
-					onSubmit={submitForm()}
-					
-          >
+					target="hiddenFrame">
 
 					<div className="flex flex-col md:flex-row justify-center mb-4">
 						<input
@@ -39,8 +37,9 @@ function App() {
 							name="member[subscribe]"
 							id="member_submit"
 							className="bg-gradient-to-r from-amber-200 to-yellow-600 hover:from-amber-200 hover:to-yellow-500 md:rounded-tl-none md:rounded-bl-none text-gray-700 rounded-full text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 font-bold uppercase cursor-pointer hover:opacity-85 duration-150"
+							onSubmit={submitForm()}
 						> Join Waitlist </button> 
-            
+
 					</div>
 					<div className="opacity-75 italic">
 						By joining the waitlist, you can gain an upperhand over other dropshippers

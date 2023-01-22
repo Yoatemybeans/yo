53a55f23191b84b0d3fd8759561fe1c33fcd3aaf
Yoatemybeans <79612361+Yoatemybeans@users.noreply.github.com>
2023-01-22 13:08:19 -0500

    Add files via upload

---
+++ careers.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Careers from './careersapp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('careers')).render(
+  <React.StrictMode>
+    <Careers/>
+  </React.StrictMode>,
+)
